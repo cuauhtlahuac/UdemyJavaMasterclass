62373f44ce4931ad4177e27c15908d17ef2c52e5
Rodrigo <cuitlagua@gmail.com>
2020-07-01 00:04:24 -0500

    double challenge done

---
+++ FloatAndDouble/src/academy/learnprogramming/Challenge.java
@@ -0,0 +1,8 @@
+package academy.learnprogramming;
+
+public class Challenge {
+    public static void challenge(String[] args) {
+        /* Convert a given number of pounds to kilograms*/
+        System.out.println("hello there");
+    }
+}
--- FloatAndDouble/src/academy/learnprogramming/Main.java
+++ FloatAndDouble/src/academy/learnprogramming/Main.java
@@ -1,6 +1,7 @@
 package academy.learnprogramming;
 
 import javax.sound.midi.Soundbank;
+import java.math.BigDecimal;
 import java.net.SocketTimeoutException;
 
 public class Main {
@@ -11,14 +12,14 @@ public class Main {
         System.out.println("Float min value:= " + myMinFloat);
         System.out.println("Float max value:= " + myMaxFloat);
 
-        Double myMaxDouble = Double.MAX_VALUE;
-        Double myMinDouble = Double.MIN_VALUE;
+        double myMaxDouble = Double.MAX_VALUE;
+        double myMinDouble = Double.MIN_VALUE;
         System.out.println("Double min value:= " + myMinDouble);
         System.out.println("Double max value:= " + myMaxDouble);
 
         int myIntValue = (int) 5.51; // makes a round floor number
         float myFloatValue = (int) 5.6565; // round floor number but with .0
-        Double myDoubleValue = 6.765; // By default the decimal numbers are doubles
+        double myDoubleValue = 6.765; // By default the decimal numbers are doubles
         System.out.println("my int value: ".concat(String.valueOf(myIntValue)));
         System.out.println("my Float number: " + myFloatValue);
         System.out.println("my Double number: " + myDoubleValue);
@@ -31,6 +32,31 @@ public class Main {
         System.out.println("MyFloatNumber: " + MyFloatNumber );
         System.out.println("myDoubleNumber: " + myDoubleNumber );
 
+        PoundsToKilograms.challenge();
+
+        /* java has a class called BigDecimal for more precision*/
+
+    }
+
+
+}
+
+class PoundsToKilograms {
+    public static void challenge() {
+    /* Try until you can do it!!!*/
+    System.out.println("\n\n" + "Try until you can do it!!!" + "\n");
+    /* Convert a given number of pounds to kilograms*/
+    double pounds = 200d;
+    double kilograms = 0.45359237d;
+    /* HINT: 1 pound = 0.45359237 kg */
+    double result = pounds * kilograms;
+    double result2 = pounds / 2.2046d; // Other way, but is not the same due the amount of float numbers
+
+        System.out.println("result 1: " + result + "\nresult 2: " + result2);
+
+    /* NOTE: 1 KG = 2.20462 pounds */
+
     }
 }
+
 
