82e61c3b123f70df9580023c543e6db2c2ab42ee
Rodrigo <cuitlagua@gmail.com>
2020-07-06 23:32:34 -0500

    if statement without block is confusing

---
+++ IfStatement/src/academy/learnprogramming/Main.java
@@ -0,0 +1,11 @@
+package academy.learnprogramming;
+
+public class Main {
+
+    public static void main(String[] args) {
+	 boolean isAlien = false;
+	 if (isAlien == true) // Don´t put a semicolon here because the next line will be ignore!!
+         System.out.println("It is not an alien");
+         System.out.println("And I´m scared of aliens");
+    }
+}
